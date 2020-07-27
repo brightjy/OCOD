ed789c81dc2579944b734a5579b63c7108b29e22
brightjy <barkji0@naver.com>
2020-07-27 23:19:19 +0900

    React JS - about jsx, props

--- movie_app_2020/src/App.js
+++ movie_app_2020/src/App.js
@@ -1,17 +1,57 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-function Food(props){
-  return (<p>I like {props.favorite}</p>);
+function Food({name, picture, rating}){
+  return (
+    <div>
+      <h2>I Like {name}</h2>
+      <h4>{rating}/5.0</h4>
+      <img src={picture} alt={name} />
+    </div>
+  );
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+};
+
+const foodILike = [
+  {
+    id: 1, // 유일한 key 값이 필요하다.
+    name: "GodKimchi",
+    image:
+      "https://th3.tmon.kr/thumbs/image/db0/e31/cba/73dc2c36c_700x700_95_FIT.jpg",
+    rating: 5.0 
+    },
+  {
+    id: 2, 
+    name: "Ramyun",
+    image:
+      "https://img.hankyung.com/photo/201902/AA.19048482.1.jpg",
+    rating: 4.9
+  },
+  {
+    id: 3,
+    name: "DduckboKKi",
+    image:
+      "https://funshop.akamaized.net/products/0000067786/vs_image800.jpg",
+    rating: 4.8
+  }
+];
 
 function App() {
   return (
     <div>
-      <h1>Hello!!!</h1>
-      <Food favorite="kimchi"/> 
-      <Food favorite="ramen"/>
-      <Food favorite="gogi"/>
-      <Food favorite="hue"/>
+      {foodILike.map(dish => (
+        <Food 
+          key={dish.id}  
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+          />
+      ))}
     </div>
   );
 }
