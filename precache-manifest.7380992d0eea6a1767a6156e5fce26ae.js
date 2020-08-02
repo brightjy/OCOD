8f368e084af2a14edcae37fe1410dd4790fd17e7
brightjy <barkji0@naver.com>
2020-08-02 09:42:30 +0900

    Updates

---
+++ precache-manifest.7380992d0eea6a1767a6156e5fce26ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9da4250ed9d4100feff0b38f2d07eaae",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "88daca0144fc9b3d8e5a",
+    "url": "/movie_app_2020/static/css/main.1ac2ed0f.chunk.css"
+  },
+  {
+    "revision": "3dd18e87ab088f0503d7",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.56ed097a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88daca0144fc9b3d8e5a",
+    "url": "/movie_app_2020/static/js/main.a4409eee.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
