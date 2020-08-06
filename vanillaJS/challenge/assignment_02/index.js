defc4f1b2507613b30dc98e0ddc262178f485144
brightjy <barkji0@naver.com>
2020-08-07 00:52:46 +0900

    resize, eventListener, classList etc

--- vanillaJS/challenge/assignment_02/index.js
+++ vanillaJS/challenge/assignment_02/index.js
@@ -1,15 +1,32 @@
 
 const body = document.querySelector("body");
-body.style.backgroundColor = "#9b59b6";
 
+const DEFAULT_CLASS = "default";
 const LARGE_CLASS = "large";
 const SMALL_CLASS = "small";
 
 function changeColor(currentWidth) {
   if (currentWidth > 700) {
-    body.classList.add(LARGE_CLASS);
+      if (body.classList.contains(SMALL_CLASS)) {
+          body.classList.remove(SMALL_CLASS);
+      } else {
+          body.classList.remove(DEFAULT_CLASS);
+      }
+      body.classList.add(LARGE_CLASS);
+  } else if (currentWidth < 500) {
+      if (body.classList.contains(LARGE_CLASS)) {
+          body.classList.remove(LARGE_CLASS);
+      } else {
+          body.classList.remove(DEFAULT_CLASS);
+      } 
+      body.classList.add(SMALL_CLASS);
   } else {
-    body.classList.add(SMALL_CLASS);
+      if (body.classList.contains(LARGE_CLASS)) {
+          body.classList.remove(LARGE_CLASS);  
+      } else {
+          body.classList.remove(SMALL_CLASS);
+      }
+      body.classList.add(DEFAULT_CLASS);  
   }
 }
 
@@ -21,13 +38,18 @@ const eventHandler = {
 };
 
 function addHello() {
-  body.innerHTML = "<h2>Hello!</h2>";
-  const h2 = body.querySelector("h2");
-  h2.style.color = "#ecf0f1";
+    body.innerHTML = "<h2>Hello!</h2>";
+    const h2 = body.querySelector("h2");
+    h2.style.color = "#ecf0f1";
+}
+
+function defaultSetting(){
+    body.classList.add(DEFAULT_CLASS);
+    addHello();
 }
 
 function init() {
-  addHello();
+  defaultSetting();
   window.addEventListener("resize", eventHandler.getInnerWidth);
 }
 
