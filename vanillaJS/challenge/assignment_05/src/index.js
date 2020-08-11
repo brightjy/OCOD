1030bf04ab7d47066fe4900d922755e5657b4158
brightjy <barkji0@naver.com>
2020-08-11 22:34:32 +0900

    not yet...

--- vanillaJS/challenge/assignment_05/src/index.js
+++ vanillaJS/challenge/assignment_05/src/index.js
@@ -1,92 +1,129 @@
-const addTaskForm = document.querySelector(".js-form"),
-    taskInput = addTaskForm.querySelector("input"),
-    pendingList = document.querySelector(".js-pending"),
-    finishedList = document.querySelector(".js-finished");
+const toDoForm = document.querySelector(".js-form"),
+    toDoInput = toDoForm.querySelector("input"),
+    toDoList = document.querySelector(".js-toDoList"),
+    finishList = document.querySelector(".js-doneList");
 
-const PENDING_LS = "pendingTasks";
+const TODOS_LS = "toDos";
+const DONES_LS = "dones";
 
+let toDos = [];
+let dones = [];
 
-/* pending tasks, finished tasks 배열 선언 
-*  tasks 는 계속 변하니까 const 말고 let 선언
-*/
-
-let pendingTasks = [];
-
-
-/* LocalStorage에 저장하는 함수 */
-function savePendingsTask(){
-    localStorage.setItem(PENDING_LS, JSON.stringify(pendingTasks));
-}
-
-/* 삭제하는 함수 */
-function deleteTasks(event) {
-    const button = event.target;
-    const li = button.parentNode;
-    pendingList.removeChild(li);
-    const cleanTasks = pendingTasks.filter(function(task){
-        return task.id !== parseInt(li.id);
+function deleteToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
     });
-    pendingTasks = cleanTasks;
-    savePendingsTask();
+    toDos = cleanToDos;
+    saveToDos();
 }
 
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
 
-/* 리스트 보여주는 함수 */
-function paintPendingTasks(text) {
+function paintDones(text) {
     const li = document.createElement("li");
-    const deleteButton = document.createElement("button");
-    const finishedButton = document.createElement("button");
+    const delBtn = document.createElement("button");
+    const doneBtn = document.createElement("button");
     const span = document.createElement("span");
-    const newId = pendingTasks.length + 1;
+    const newId = toDos.length + 1;
 
-    deleteButton.innerHTML = "X";
-    deleteButton.addEventListener("click", deleteTasks);
-
-    finishedButton.innerHTML = "V";
-    finishedButton.addEventListener("click", addFinishedTasks);
+    delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", deleteDones);
 
     span.innerText = text;
-    li.appendChild(deleteButton);
-    li.appendChild(finishedButton);
+    li.appendChild(delBtn);
+    li.appendChild(doneBtn);
+    li.appendChild(span);
     li.id = newId;
-    pendingList.appendChild(li);
-    const taskObj = {
+    finishList.appendChild(li);
+    const done = {
         text: text,
         id: newId
     };
+    dones.push(done);
+    saveDones();
 
-    pendingTasks.push(taskObj);
-    savePendingsTask();
 }
 
-/* submit 이벤트 핸들러 함수 */
+function doneToDo(event) {
+    const btn = event.target;
+    const li = btn.parentNode;
+    toDoList.removeChild(li);
+    const cleanToDos = toDos.filter(function(toDo) {
+        return toDo.id !== parseInt(li.id);
+    });
+    const donedToDos = toDos.filter(function(toDo) {
+        return toDo.id == parseInt(li.id);
+    });
+    toDos = cleanToDos;
+    dones = donedToDos;
+    paintDones(dones.text);
+    saveToDos();
+}
+
+function paintToDo(text) {
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const doneBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+
+    delBtn.innerHTML = "X";
+    delBtn.addEventListener("click", deleteToDo);
+
+    doneBtn.innerHTML = "V";
+    doneBtn.addEventListener("click", doneToDo);
+
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(doneBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDo = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDo);
+    saveToDos();
+
+}
+
 function handleSubmit(event) {
     event.preventDefault();
-    const currentValue = taskInput.value;
-    paintTasks(currentValue);
-    taskInput.value = "";
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
 }
 
-/* Pending 리스트 로드 함수 */
-function loadPendingList() {
-    
-    const pendingTasks = localStorage.getItem(PENDING_LS);
-
-    if (pendingTasks !== null) {
-        // LocalStorage에 String 형태로 저장된 값을 JSON으로
-        const parsedPendingTasks = JSON.parse(pendingTasks);
-        parsedPendingTasks.forEach(function(tasks){
-           paintPendingTasks(tasks.text); 
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo){
+            paintToDo(toDo.text);
         });
-
     }
+}
 
-}    
+function loadDones() {
+    const loadedDones = localStorage.getItem(DONES_LS);
+    if (loadDones !== null) {
+        const parsedDones = JSON.parse(loadedDones);
+        parsedDones.forEach(function(done) {
+            paintDones(dones.text);
+        });
+    }
+}
 
 function init() {
 
-    loadPendingList();
-    addTaskForm.addEventListener("submit", handleSubmit);
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
 
 }
 
