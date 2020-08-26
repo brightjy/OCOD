f51e92ec22da72f215d7270efb4142c927227bda
brightjy <barkji0@naver.com>
2020-08-26 22:17:22 +0900

    8/26 commit

--- boostCourse/Algorithm/arrayNotDuplicated.c
+++ boostCourse/Algorithm/arrayNotDuplicated.c
@@ -1,47 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-// N개의 정수가 주어진다. 이때, N개의 정수를 오름차순으로 정렬하는 프로그램을 작성하시오.
-// 같은 정수는 한번만 출력한다.
-
-// 1. 오름차순 정렬, Big-O: O(N*log(N))
-
-// 1-(1) 힙상태 만들기
-void makeHeapState ( int *arr, int size )
+enum
 {
-    for ( int i = 1; i < size ; i++ )
-    {
-        int child = i;
-        do
-        {
-            // 자식 노드가 부모 노드보다 크면 교환
-            int root = ( child - 1) / 2;
-            if ( arr[root] < arr[child] )
-            {
-                int temp = arr[root];
-                arr[root] = arr[child];
-                arr[child] = temp;
-            }
-            child = root;
-        }
-        while ( child != 0 );
-    }
-}
+    False, True
+};
 
-// 1-(2) 루트와 최하단 노드 교체
-void heap ( int *arr, int *size)
-{
-    int temp = arr[0];
-    arr[0] = arr[*size - 1];
-    arr[*size - 1] = temp;
-    --(*size);
-}
-
-// 2. 중복제거
-
-
-
-
+void removeDuplicates ( int *arr, int *size );
+int searchKeyInArr(int *arr, int size, int key);
+void ascendArr ( int *arr, int size );
+void printArr ( int *arr, int size );
 
 
 int main()
@@ -49,18 +18,82 @@ int main()
     int size = 10;
     int arr[10] = {5,5,7,1,9,3,2,8,1,1};
 
-    for ( int i = 0; i < size; i++ )
-    {
-        makeHeapState(arr, size);
-        heap(arr, &size);
-    }
 
-
-
-    for ( int i = 0; i < 10; i++ )
-    {
-        printf("%d", arr[i]);
-    }
+    removeDuplicates(arr, &size); // 중복 제거 O(n^2)
+    ascendArr (arr, size); // 오름차순 정열 by 버블정렬 O(n^2)
+    printArr(arr, size);
 
     return 0;
 }
+
+void removeDuplicates ( int *arr, int *size )
+{
+    int *uniqueArr = (int*)calloc(*size, sizeof(int));
+    int uniqueArrCount = 0;
+
+    for ( int i = 0; i < *size; i++ )
+    {
+        if ( !searchKeyInArr(uniqueArr, uniqueArrCount, arr[i]) )
+        {
+            uniqueArr[uniqueArrCount++] = arr[i];
+        }
+    }
+
+    memset(arr, 0, sizeof(arr[0])*(*size));
+
+    for ( int i = 0; i < uniqueArrCount; i++ )
+    {
+        arr[i] = uniqueArr[i];
+    }
+
+    if ( uniqueArr )
+    {
+        free(uniqueArr);
+        uniqueArr = NULL;
+    }
+
+    *size = uniqueArrCount;
+
+}
+
+int searchKeyInArr(int *arr, int size, int key)
+{
+    for ( int i = 0; i < size; i++ )
+    {
+        if ( arr[i] == key )
+        {
+            return True;
+        }
+    }
+    return False;
+}
+
+void ascendArr ( int *arr, int size )
+{
+    int temp;
+    for ( int i = 0; i < size; i++ )
+    {
+        for ( int j = 0; j < size-i-1; j++ )
+        {
+            if ( arr[j] > arr[j+1] )
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+
+void printArr ( int *arr, int size )
+{
+    for ( int i = 0; i < size; i++ )
+    {
+        printf("%d", arr[i]);
+    }
+    printf("\n");
+}
+
+
+
